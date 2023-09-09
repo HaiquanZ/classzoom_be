c6fd017b4c3a4935ad9512954d012c0fff4cdb13
HaiquanZ <nn764562@gmail.com>
2023-09-09 20:01:36 +0700

    done base

--- src/api/controllers/room.controller.js
+++
@@ -1,8 +0,0 @@
-const { app } = require('../../server');
-
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-io.on('connection', (socket) => {
-    console.log('ok');
-})
--- src/api/routes/room.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import * as roomController from '../controllers/room.controller';
-const router = express.Router();
-
-
-
-export default router;
